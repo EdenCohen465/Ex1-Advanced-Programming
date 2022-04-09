e0fd981c6bf0abc61f8f6f762ff37dd85b1ef9ea
Roni Oded <74676502+Ronioded@users.noreply.github.com>
2022-04-09 20:46:21 +0300

    9/4

--- ex1-advanced-programming/src/chat/Message.js
+++ ex1-advanced-programming/src/chat/Message.js
@@ -1,33 +1,21 @@
 import './Message.css';
 import React from 'react';
 
-function DisplayMessage(message) {
-    if (message.type == "text") {
-        return (<span className="text">{message.m}</span>);
-    } else if (message.type == "photo") {
-        return (<img className="photoMessage rounded float-start " src={URL.createObjectURL(message.m)} alt="photo" ></img>);
-    } else if (message.type == "video") {
-        return (<video>
-            <source src={message.m} type="video/*"></source>
-        </video>);
-    } // else voice
-}
-
 function Message({ messagesList }) {
     const chatHistory =
         messagesList.map((message, key) => {
             return (
-                <div className="message" key={key} > { (message.m != "") ? (
+                <div className="message" key={key} > { (message.message != "") ? (
                    <div> 
                        {(() => {
                             if(message.type == "text") {
-                                return(<span className="text">{message.m}</span>);
+                                return(<span className="text">{message.message}</span>);
                             } else if (message.type == "photo") {
-                                return(<img className="photoMessage rounded float-start " src={URL.createObjectURL(message.m)} alt="photo" ></img>)
+                                return (<img className="photoMessage rounded float-start " src={URL.createObjectURL(message.message)} alt="photo" ></img>)
                             } else if (message.type == "video") {
                                 return(
                                     <video className="videoMessage" controls>
-                                    <source src={URL.createObjectURL(message.m)} type="video/mp4"></source>
+                                        <source src={URL.createObjectURL(message.message)} type="video/mp4"></source>
                                 </video>);
                             }
                         })()}
